a2fbfad7a7c9b88b498d11cb8076757fd75577b4
03NoName <falaiyewisdom@gmail.com>
2023-08-12 16:45:15 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Function for the task
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char z;
+
+for (z = 'a'; z <= 'z'; z++)
+putchar(z);
+putchar('\n');
+
+return (0);
+
+}
