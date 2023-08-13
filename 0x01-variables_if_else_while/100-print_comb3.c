6985522e01412d44078e3b94f936fa8e1afaf1a7
03NoName <falaiyewisdom@gmail.com>
2023-08-13 00:39:24 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - prints all the numbers of base 16 in lowercase.
+ * main - prints all possible different combinations of two digits
  * Return: Always 0.
  */
 
 int main(void)
 {
-	int num;
-	char letter;
+	int n;
 
-	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
-
+	for (n = 48; n <= 56; n++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n)
+			{
+				putchar (n);
+				putchar (n);
+				if (n != 56 || n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
