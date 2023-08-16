0b5246470e58eb25dfdfa719c1892a92a7b2df0c
03NoName <falaiyewisdom@gmail.com>
2023-08-16 10:40:27 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Print's sum of numbers
+ * @n: Number
+ * Return: One for all
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
