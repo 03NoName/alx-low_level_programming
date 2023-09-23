e414c5f10441e61b705c9ed1957a9e5785fef50d
03NoName <falaiyewisdom@gmail.com>
2023-09-23 03:35:01 -0700

    P!

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,7 +5,7 @@
  * get_op_func - Selects the correct function to perform
  * the operation asked by the user.
  * @s: The operator passed as arguement.
- * @Return: A pointer to the function corresponding to the
+ * Return: A pointer to the function corresponding to the
  * operator given as a parameter.
  */
 
