b5653f8925b97e0355720fab7efad518a8fa82c0
03NoName <falaiyewisdom@gmail.com>
2023-09-26 15:23:25 -0700

    Push./a

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - Prints all the elements of a linked list.
+ * Return: Number of nodes.
+ * @a: Linked list of type listint_t to print.
  */
-size_t print_listint(const listint_t *h)
-{
-size_t num = 0;
 
-while (h)
+size_t print_listint(const listint_t *a)
 {
-	printf("%d\n", h->n);
-	num++;
-	h = h->next;
-}
+	size_t num = 0;
 
-return (num);
+	while (a)
+	{
+		printf("%d\n", a->n);
+		num++;
+		a = a->next;
+	}
+
+	return (num);
 }
