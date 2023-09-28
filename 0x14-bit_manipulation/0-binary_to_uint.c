9ca058478d6c286008e79aa7de95be5b1cb360ac
03NoName <falaiyewisdom@gmail.com>
2023-09-28 15:17:13 -0700

    Pushvi 101-password!

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,33 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * binary_to_uint - Converts a binary number to unsigned int.
  * Return: The converted number.
  * @b: String containing the binary number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	if (b == NULL)
+		return (NULL);
 
-	if (!b)
-		return (0);
+	int sas = 0;
+	int len = strlen(b), i;
+	int base = 1;
 
-	for (i = 0; b[i]; i++)
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
+		if (b[i] == '0' || b[i] == '1')
+		{
+			if (b[i] == '1')
+				sas += base;
+			base *= 2;
+		}
 	}
-
-	return (dec_val);
+	else
+	{
+		return (0);
+	}
+	return (sas);
 }
