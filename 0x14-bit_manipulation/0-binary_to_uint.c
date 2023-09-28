b27db96d7965223624ba73f2c6df602d83569fd8
03NoName <falaiyewisdom@gmail.com>
2023-09-28 16:11:54 -0700

    Push./a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,26 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * binary_to_uint - Converts a binary number to unsigned int.
- * Return: The converted number.
- * @b: String containing the binary number.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-		return (NULL);
+	int i;
+	unsigned int dec_val = 0;
 
-	int sas = 0;
-	int len = strlen(b), i;
-	int base = 1;
-
-	for (i = len - 1; i >= 0; i--)
-	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			if (b[i] == '1')
-				sas += base;
-			base *= 2;
-		}
-	}
-	else
-	{
+	if (!b)
 		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (sas);
+
+	return (dec_val);
 }
