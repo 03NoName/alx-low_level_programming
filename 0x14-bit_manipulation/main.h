48d7e445bbd67b055b2d9d4b3d803f708567e845
03NoName <falaiyewisdom@gmail.com>
2023-10-27 00:55:33 -0700

    push

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,10 +5,6 @@
 
 int _putchar(char c);
 
-/**
- * int _atoi(const char *s);
- */
-
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
